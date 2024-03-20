21533f68bce6a22cd2da59b1678fdb5bb67d412b
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-03-20 20:23:46 +0900

    //

--- js/main.js
+++ js/main.js
@@ -21,20 +21,18 @@ window.addEventListener('load', function () {
 function FixedAnime() {
 	var headerH = $('#header').outerHeight(true);
 	var scroll = $(window).scrollTop();
-	if (scroll >= headerH){//headerの高さ以上になったら
-			$('#header').addClass('fixed');//fixedというクラス名を付与
-		}else{//それ以外は
-			$('#header').removeClass('fixed');//fixedというクラス名を除去
+	if (scroll >= headerH){
+			$('#header').addClass('fixed');
+		}else{
+			$('#header').removeClass('fixed');
 		}
 }
 
-// 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
-	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+	FixedAnime();
 });
 
-// ページが読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load', function () {
-	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+	FixedAnime();
 });
 
