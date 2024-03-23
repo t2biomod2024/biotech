d4a3f20f290062f376e39328d37e232575a5c210
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-03-23 21:49:34 +0900

    change topic-slider

--- js/main.js
+++ js/main.js
@@ -12,18 +12,16 @@ window.addEventListener('load', function () {
 });
 
 // topic-slider
-$('.topic-slider').slick({
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    speed: 500,//スライドのスピード。初期値は300。
-    slidesToShow: 3,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    centerMode: true,//要素を中央ぞろえにする
-    variableWidth: true,//幅の違う画像の高さを揃えて表示
-    dots: true,//下部ドットナビゲーションの表示
-});
+$(function () {
+    $(".topic-slider").slick({
+      arrows: false,
+      autoplay: true,
+      adaptiveHeight: true,
+      centerMode: true,
+      centerPadding: "15%",
+      dots: true,
+    });
+  });
 
 // news-slider
 var slider;
